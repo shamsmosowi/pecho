a837e9eec3d025b7a60f6972a98226f17c591dd0
shamsmosowi <shamsmosowi@pc-10-70-53-212.student.sydney.edu.au>
2016-06-10 17:24:26 +1000

    added more element affects

--- JS/elementsActions.js
+++ JS/elementsActions.js
@@ -1,3 +1,5 @@
+
+var waveType = {"triangle":1,"sin":2,"square":3,"even":4,"saw":5}
 class Actions{
 
 linear(array,p,firstValue,lastValue){
@@ -14,15 +16,36 @@ randomize(array,p,minValue,maxValue){
   let property = p;
   array.forEach(x=>{x[property] = random(minValue,maxValue)});
 }
-alternate(array,p,minValue,maxValue,steps){
+alternate(array,p,minValue,maxValue,steps,type){
 //use a sine function to alternate value
 //requires a min and max, and step size(period)
 pushToUndos();
   let property = p;
 let d = (maxValue - minValue)/2;//midpoint(vertical shift and Amplitude)
 let k = steps/(2*PI);
+
 let index = 0;
-array.forEach(x=>{x[property] = d*Math.sin(k*index)+minValue;index+=1});
+console.log(steps);///
+
+console.log(index);
+console.log(steps);
+if (type === waveType.triangle){
+array.forEach(x=>{x[property] = ((maxValue -minValue)/4)+2*abs((index%steps)-(steps/2))-(steps/4);console.log(x[property]);;index+=1});
+}else if(type === waveType.sin){
+  array.forEach(x=>{x[property] = d*Math.sin(k*index)+minValue;index+=1});
+}else if(type === waveType.even){
+  array.forEach(x=>{if(index%2){x[property] = minValue}else{x[property] = maxValue};index+=1});
+}else if(type === waveType.saw){
+  array.forEach(x=>{x[property] = minValue+(maxValue-minValue)*(index%steps)/steps;index+=1});
+}else if(type === waveType.saw){
+  array.forEach(x=>{x[property] = minValue+(maxValue-minValue)*(index%steps)/steps;index+=1});
+}
+}
+padding(array,property,padding){
+  pushToUndos();
+  let index = 0;
+  array.forEach(x=>{x[property] = x[property] + padding*index;index+=1});
+
 
 }
 exponational(){
@@ -41,7 +64,11 @@ align(direction){
 
 }
 
-noise(){
+pnoise(array,p,minValue,maxValue){
+//perlin noise
+pushToUndos();
+let property = p;
+array.forEach(x=>{x[property] = minValue+(maxValue-minValue)*(noise(minValue,maxValue))});
 
 }
 }
